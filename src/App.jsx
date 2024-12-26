471d7bc57898d010346f6e6a5dc7626d29593e38
pasindu <gallagepasinduhansana@gmail.com>
2024-12-27 01:11:16 +0530

    Tab & Mobile responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,87 @@
 import "./App.css";
 import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-  useNavigate,
+	BrowserRouter,
+	Routes,
+	Route,
+	Navigate,
+	useNavigate,
 } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Admin from "./Pages/Admin/Admin";
 import Login from "./Pages/Admin/Login";
 import PrivateRoute from "./Services/PrivateRoute";
 import Home from "./Pages/Home";
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
 import SplashScreen from "./Components/SplashScreen";
 import PreLoader from "./Components/PreLoader";
 import { useState, useEffect } from "react";
 import { AuthProvider, useAuth } from "./Services/AuthContex";
 import Scroll from "./Components/Scroll";
 import AboutUs from "./Components/AboutUs";
+import Gallery_Expanded from "./Components/Gallery_Expanded";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isLanguageSelected, setIsLanguageSelected] = useState(false);
-  const [isPreLoading, setIsPreLoading] = useState(true);
-  const navigate = useNavigate();
-  const isAuth = useAuth();
+	const [isLoading, setIsLoading] = useState(true);
+	const [isLanguageSelected, setIsLanguageSelected] = useState(false);
+	const [isPreLoading, setIsPreLoading] = useState(true);
+	const navigate = useNavigate();
+	const isAuth = useAuth();
 
-  useEffect(() => {
-    const savedLanguage = localStorage.getItem("language");
-    if (savedLanguage) {
-      setIsLoading(false);
-    } else {
-      setIsLoading(true);
-    }
-  }, []);
+	useEffect(() => {
+		const savedLanguage = localStorage.getItem("language");
+		if (savedLanguage) {
+			setIsLoading(false);
+		} else {
+			setIsLoading(true);
+		}
+	}, []);
 
-  const handleLanguageChange = (lang) => {
-    localStorage.setItem("language", lang);
-    setIsLanguageSelected(true);
-    setTimeout(() => {
-      setIsLoading(false);
-      navigate("/home"); // Navigate to home without parsing the URL string
-    }, 3000); // Adjust the duration as needed
-  };
+	const handleLanguageChange = (lang) => {
+		localStorage.setItem("language", lang);
+		setIsLanguageSelected(true);
+		setTimeout(() => {
+			setIsLoading(false);
+			navigate("/home"); // Navigate to home without parsing the URL string
+		}, 3000); // Adjust the duration as needed
+	};
 
-  const handlePreLoader = () => {
-    setIsPreLoading(false);
-  };
+	const handlePreLoader = () => {
+		setIsPreLoading(false);
+	};
 
-  if (isPreLoading) {
-    return <PreLoader onLoaded={handlePreLoader} />;
-  }
+	if (isPreLoading) {
+		return <PreLoader onLoaded={handlePreLoader} />;
+	}
 
-  if (isLoading && !isLanguageSelected) {
-    return <SplashScreen handleLanguageChange={handleLanguageChange} />;
-  }
+	if (isLoading && !isLanguageSelected) {
+		return <SplashScreen handleLanguageChange={handleLanguageChange} />;
+	}
 
-  return (
-    <div className="app bg-primarybg text-primarytext">
-      <Toaster position="top-center" containerStyle={{ top: 60 }} />
-      <Routes>
-        <Route
-          path="/"
-          element={<SplashScreen handleLanguageChange={handleLanguageChange} />}
-        />
-        <Route path="/home" element={<Home />} />
-        {/* Admin Routes */}
-        <Route path="/admin" element={<Login />} />
-        <Route element={<PrivateRoute isAuth={isAuth} />}>
-          <Route path="admin/dashboard" element={<Admin />} />
-        </Route>
-      </Routes>
-    </div>
-  );
+	return (
+		<div className="app bg-primarybg text-primarytext">
+			<Toaster position="top-center" containerStyle={{ top: 60 }} />
+			<Routes>
+				<Route
+					path="/"
+					element={<SplashScreen handleLanguageChange={handleLanguageChange} />}
+				/>
+				<Route path="/home" element={<Home />} />
+				<Route path="/gallery" element={<Gallery_Expanded />} />
+				{/* Admin Routes */}
+				<Route path="/admin" element={<Login />} />
+				<Route element={<PrivateRoute isAuth={isAuth} />}>
+					<Route path="admin/dashboard" element={<Admin />} />
+				</Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default function AppWrapper() {
-  return (
-    <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
-  );
+	return (
+		<AuthProvider>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</AuthProvider>
+	);
 }
--- src/Components/Elements/Buttons.jsx
+++ src/Components/Elements/Buttons.jsx
@@ -3,123 +3,126 @@ import { IoArrowForward } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
 
 const PrimaryButton = ({ TextContent }) => {
-  return (
-    <div className="flex items-center justify-center">
-      <button
-        className="flex flex-row items-center justify-center text-primarytextwhite bg-gradient-to-r from-primary1 to-primary2  hover:scale-[1.02] rounded-[8px] h-[48px] w-auto hover:text-white transition-all duration-200 px-2"
-        onClick={() =>
-          window.open("https://charithamunasinghe.com/login", "_blank")
-        }
-      >
-        {TextContent}
-        <IoArrowForward className="ml-2 w-6 h-6 mb-[0.5]" />
-      </button>
-    </div>
-  );
+	return (
+		<div className="flex items-center justify-center">
+			<button
+				className="flex flex-row items-center justify-center text-primarytextwhite bg-gradient-to-r from-primary1 to-primary2  hover:scale-[1.02] rounded-[8px] h-[48px] w-auto hover:text-white transition-all duration-200 px-2"
+				onClick={() =>
+					window.open("https://charithamunasinghe.com/login", "_blank")
+				}
+			>
+				{TextContent}
+				<IoArrowForward className="ml-2 w-6 h-6 mb-[0.5]" />
+			</button>
+		</div>
+	);
 };
 
-const SecondaryButton = ({ TextContent }) => {
-  return (
-    <div className="flex items-center justify-center">
-      <button className="text-primary1 border-2 bg-white border-primary1  hover:scale-[1.02] rounded-[8px] h-[48px] w-auto hover:text-primaryHover2 transition-all duration-200 px-2">
-        {TextContent}
-      </button>
-    </div>
-  );
+const SecondaryButton = ({ TextContent, onclickevent }) => {
+	return (
+		<div className="flex items-center justify-center">
+			<button
+				className="text-primary1 border-2 bg-white border-primary1  hover:scale-[1.02] rounded-[8px] h-[48px] w-auto hover:text-primaryHover2 transition-all duration-200 px-2"
+				onClick={onclickevent}
+			>
+				{TextContent}
+			</button>
+		</div>
+	);
 };
 
 const SubmitButton = ({ TextContent }) => {
-  return (
-    <div className="flex items-center justify-center">
-      <button className="text-primarytextwhite bg-gradient-to-r from-primary1 to-primary2  hover:scale-[1.02] rounded-[5px] h-[50px] w-[145px] hover:text-white transition-all duration-200">
-        {TextContent}
-      </button>
-    </div>
-  );
+	return (
+		<div className="flex items-center justify-center">
+			<button className="text-primarytextwhite bg-gradient-to-r from-primary1 to-primary2  hover:scale-[1.02] rounded-[5px] h-[50px] w-[145px] hover:text-white transition-all duration-200">
+				{TextContent}
+			</button>
+		</div>
+	);
 };
 
 const SecondarySubmitButton = ({ TextContent }) => {
-  return (
-    <div className="flex items-center justify-center">
-      <button className="text-primary1 border-2 bg-white border-primary1  hover:scale-[1.02] rounded-[5px] h-[48px] w-[145px] hover:text-primaryHover2 transition-all duration-200">
-        {TextContent}
-      </button>
-    </div>
-  );
+	return (
+		<div className="flex items-center justify-center">
+			<button className="text-primary1 border-2 bg-white border-primary1  hover:scale-[1.02] rounded-[5px] h-[48px] w-[145px] hover:text-primaryHover2 transition-all duration-200">
+				{TextContent}
+			</button>
+		</div>
+	);
 };
 
 const SinhalaButton = ({ TextContent }) => {
-  return (
-    <div className="flex items-center justify-center">
-      <button className="text-primary1 text-[14px] border-2 bg-white border-primary1  hover:scale-[1.02] rounded-[5px] h-[30px] w-[30px] hover:text-primaryHover2 transition-all duration-200">
-        සිං
-      </button>
-    </div>
-  );
+	return (
+		<div className="flex items-center justify-center">
+			<button className="text-primary1 text-[14px] border-2 bg-white border-primary1  hover:scale-[1.02] rounded-[5px] h-[30px] w-[30px] hover:text-primaryHover2 transition-all duration-200">
+				සිං
+			</button>
+		</div>
+	);
 };
 
 const EnglishButton = ({ TextContent }) => {
-  return (
-    <div className="flex items-center justify-center">
-      <button className="text-primary1 text-[12px] border-2 bg-white border-primary1  hover:scale-[1.02] rounded-[5px] h-[30px] w-[30px] hover:text-primaryHover2 transition-all duration-200">
-        Eng
-      </button>
-    </div>
-  );
+	return (
+		<div className="flex items-center justify-center">
+			<button className="text-primary1 text-[12px] border-2 bg-white border-primary1  hover:scale-[1.02] rounded-[5px] h-[30px] w-[30px] hover:text-primaryHover2 transition-all duration-200">
+				Eng
+			</button>
+		</div>
+	);
 };
 
 const LanguageToggleButton = ({ onLanguageChange, language }) => {
-  useEffect(() => {
-    const savedLanguage = localStorage.getItem("language");
-    if (savedLanguage) {
-      document.documentElement.lang = savedLanguage; // Set the initial language
-    }
-  }, []);
+	useEffect(() => {
+		const savedLanguage = localStorage.getItem("language");
+		if (savedLanguage) {
+			document.documentElement.lang = savedLanguage; // Set the initial language
+		}
+	}, []);
 
-  const handleLanguageChange = (lang) => {
-    onLanguageChange(lang);
-    console.log(`Language changed to: ${lang}`);
-    document.documentElement.lang = lang; // Change the web language
-    localStorage.setItem("language", lang); // Save the language to local storage
-    if (window.location.pathname !== "/") {
-      window.location.reload(true); // Reload the page if not on the SplashScreen
-    }
-  };
+	const handleLanguageChange = (lang) => {
+		onLanguageChange(lang);
+		console.log(`Language changed to: ${lang}`);
+		document.documentElement.lang = lang; // Change the web language
+		localStorage.setItem("language", lang); // Save the language to local storage
+		if (window.location.pathname !== "/") {
+			window.location.reload(true); // Reload the page if not on the SplashScreen
+		}
+	};
 
-  const isEnglish = language === "en";
+	const isEnglish = language === "en";
 
-  return (
-    <div className="flex items-center justify-center gap-1 ">
-      <button
-        className={`text-primary1 text-[14px] border-2 border-primary1 hover:scale-[1.02] rounded-[5px] h-[30px] w-[30px]  transition-all duration-200 ${
-          isEnglish
-            ? "bg-gradient-to-r from-primary1 to-primary2 text-white"
-            : " bg-white"
-        }`}
-        onClick={() => handleLanguageChange("en")}
-      >
-        {isEnglish ? "En" : "En"}
-      </button>
-      <button
-        className={`text-primary1 text-[14px] border-2  border-primary1 hover:scale-[1.02] rounded-[5px] h-[30px] w-[30px]  transition-all duration-200 ${
-          !isEnglish
-            ? "bg-gradient-to-r from-primary1 to-primary2 text-white"
-            : " bg-white"
-        }`}
-        onClick={() => handleLanguageChange("si")}
-      >
-        {isEnglish ? "සිං" : "සිං"}
-      </button>
-    </div>
-  );
+	return (
+		<div className="flex items-center justify-center gap-1 ">
+			<button
+				className={`text-primary1 text-[14px] border-2 border-primary1 hover:scale-[1.02] rounded-[5px] h-[30px] w-[30px]  transition-all duration-200 ${
+					isEnglish
+						? "bg-gradient-to-r from-primary1 to-primary2 text-white"
+						: " bg-white"
+				}`}
+				onClick={() => handleLanguageChange("en")}
+			>
+				{isEnglish ? "En" : "En"}
+			</button>
+			<button
+				className={`text-primary1 text-[14px] border-2  border-primary1 hover:scale-[1.02] rounded-[5px] h-[30px] w-[30px]  transition-all duration-200 ${
+					!isEnglish
+						? "bg-gradient-to-r from-primary1 to-primary2 text-white"
+						: " bg-white"
+				}`}
+				onClick={() => handleLanguageChange("si")}
+			>
+				{isEnglish ? "සිං" : "සිං"}
+			</button>
+		</div>
+	);
 };
 
 export {
-  PrimaryButton,
-  SubmitButton,
-  SecondaryButton,
-  SecondarySubmitButton,
-  SinhalaButton,
-  EnglishButton,
-  LanguageToggleButton,
+	PrimaryButton,
+	SubmitButton,
+	SecondaryButton,
+	SecondarySubmitButton,
+	SinhalaButton,
+	EnglishButton,
+	LanguageToggleButton,
 };
