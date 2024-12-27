0e9760a99bd329eed7b36424950d352f03363917
chathuwa-whiz <navodchathushka@gmail.com>
2024-12-27 12:15:47 +0530

    Merge branch 'PasiDev' of https://github.com/Pasinduhansana/BIOZONE_Latest into Production

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,63 +10,65 @@ import Footer from "../Components/Footer";
 import Navigation from "../Components/Navigation";
 import Map from "../Components/Map";
 import VortextComponent from "../Components/VortextComponent";
+import Text from "../Components/Elements/Text";
 
 const Home = () => {
-  // Define refs for each section
-  const homeRef = useRef(null);
-  const locationsRef = useRef(null);
-  const galleryRef = useRef(null);
-  const contactRef = useRef(null);
+	// Define refs for each section
+	const homeRef = useRef(null);
+	const locationsRef = useRef(null);
+	const galleryRef = useRef(null);
+	const contactRef = useRef(null);
 
-  const location = useLocation();
+	const location = useLocation();
 
-  useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    const lang = params.get("lang");
-    if (lang) {
-      localStorage.setItem("language", lang);
-    }
-  }, [location]);
+	useEffect(() => {
+		const params = new URLSearchParams(location.search);
+		const lang = params.get("lang");
+		if (lang) {
+			localStorage.setItem("language", lang);
+		}
+	}, [location]);
 
-  return (
-    <div className="flex flex-col w-screen overflow-hidden">
-      {/* Pass the refs to the corresponding sections */}
-      <Navigation
-        homeRef={homeRef}
-        locationsRef={locationsRef}
-        galleryRef={galleryRef}
-        contactRef={contactRef}
-      />
-      <div ref={homeRef}>
-        <Landing />
-      </div>
-      <div ref={locationsRef}>
-        <Map />
-      </div>
-      <div ref={galleryRef}>
-        <Gallery />
-      </div>
-      <FAQ />
-      {/* <AboutUs /> */}
-      <Advertisement />
-      <div ref={contactRef}>
-        <ContactUs />
-      </div>
-      {/* <Footer
+	return (
+		<div className="flex flex-col w-screen overflow-hidden">
+			{/* Pass the refs to the corresponding sections */}
+			<Navigation
+				homeRef={homeRef}
+				locationsRef={locationsRef}
+				galleryRef={galleryRef}
+				contactRef={contactRef}
+			/>
+			<div ref={homeRef}>
+				<Landing />
+			</div>
+			<div ref={locationsRef}>
+				<Map />
+			</div>
+			<div ref={galleryRef}>
+				<Gallery />
+			</div>
+			<FAQ />
+			{/* <AboutUs /> */}
+			<Text />
+			{/* <Advertisement /> */}
+			<div ref={contactRef}>
+				<ContactUs />
+			</div>
+			{/* <Footer
         className="absolute bottom-0 left-0 w-screen"
         homeRef={homeRef}
         locationsRef={locationsRef}
         galleryRef={galleryRef}
         contactRef={contactRef}
       /> */}
-      <VortextComponent
-        homeRef={homeRef}
-        locationsRef={locationsRef}
-        galleryRef={galleryRef}
-        contactRef={contactRef}
-      />
-    </div>
-  );
+			<VortextComponent
+				homeRef={homeRef}
+				locationsRef={locationsRef}
+				galleryRef={galleryRef}
+				contactRef={contactRef}
+			/>
+		</div>
+	);
 };
 
 export default Home;
--- src/content/AboutUsContent.js
+++ src/content/AboutUsContent.js
@@ -1,60 +1,46 @@
 // aboutUsContent.js
 const aboutUsContent = {
   en: {
-    sections: [
-      {
-        title: "Where",
-        description:
-          "Now through this web site, he is ready to guide you towards your academic goals with personalized support and resources.",
-      },
-      {
-        title: "Biology and",
-        description: "Biology and",
-      },
-      {
-        title: "Technology",
-        description:
-          "His dedication and expertise have made him a trusted mentor for Advanced Level students.",
-      },
-      {
-        title: "meet",
-        description:
-          "Graduating from the University of Peradeniya, Dr. Charitha Munasinghe has over 10 years of experience in teaching biology and helping students gain admission to Sri Lanka's top state universities.",
-      },
-      {
-        title: "",
-        description:
-          "His dedication and expertise have made him a trusted mentor for Advanced Level students.",
-      },
-    ],
+    topic: "About Us",
+    title1: "Where",
+    description1:
+      "Now through this website, he is ready to guide you towards your academic goals with personalized support and resources.",
+
+    title2: "Biology and",
+    description2: "Biology and",
+
+    title3: "Technology",
+    description3:
+      "His dedication and expertise have made him a trusted mentor for Advanced Level students.",
+
+    title4: "Meet",
+    description4:
+      "Graduating from the University of Peradeniya, Dr. Charitha Munasinghe has over 10 years of experience in teaching biology and helping students gain admission to Sri Lanka's top state universities.",
+
+    title5: "",
+    description5:
+      "His dedication and expertise have made him a trusted mentor for Advanced Level students.",
   },
   si: {
-    sections: [
-      {
-        title: "කොහෙද",
-        description:
-          "දැන් මෙම වෙබ් අඩවිය හරහා, ඔහු ඔබේ අධ්‍යාපනික ඉලක්ක කරා මාර්ගෝපදේශනය කිරීමට පුද්ගලික සහාය සහ සම්පත් සමඟ සූදානම් වේ.",
-      },
-      {
-        title: "ජීව විද්‍යාව සහ",
-        description: "ජීව විද්‍යාව සහ",
-      },
-      {
-        title: "තාක්ෂණය",
-        description:
-          "ඔහුගේ කැපවීම සහ විශේෂඥතාවය ඔහුව උසස් පෙළ සිසුන් සඳහා විශ්වාසනීය උපදේශකයෙකු කර ඇත.",
-      },
-      {
-        title: "හමුවන්න",
-        description:
-          "පේරාදෙණිය විශ්වවිද්‍යාලයෙන් උපාධිය ලබා ඇති, ආචාර්ය චරිත් මුනසිංහට ජීව විද්‍යාව ඉගැන්වීමේ සහ ශ්‍රී ලංකාවේ ප්‍රමුඛ රාජ්‍ය විශ්වවිද්‍යාලවලට සිසුන් ඇතුළත් කිරීමට උදව් කිරීමේ වසර 10 කට වැඩි පළපුරුද්දක් ඇත.",
-      },
-      {
-        title: "",
-        description:
-          "ඔහුගේ කැපවීම සහ විශේෂඥතාවය ඔහුව උසස් පෙළ සිසුන් සඳහා විශ්වාසනීය උපදේශකයෙකු කර ඇත.",
-      },
-    ],
+    topic: "අපි කවුද",
+    title1: "කොහෙද",
+    description1:
+      "දැන් මෙම වෙබ් අඩවිය හරහා, ඔහු ඔබේ අධ්‍යාපනික ඉලක්ක කරා මාර්ගෝපදේශනය කිරීමට පුද්ගලික සහාය සහ සම්පත් සමඟ සූදානම් වේ.",
+
+    title2: "ජීව විද්‍යාව සහ",
+    description2: "ජීව විද්‍යාව සහ",
+
+    title3: "තාක්ෂණය",
+    description3:
+      "ඔහුගේ කැපවීම සහ විශේෂඥතාවය ඔහුව උසස් පෙළ සිසුන් සඳහා විශ්වාසනීය උපදේශකයෙකු කර ඇත.",
+
+    title4: "හමුවන්න",
+    description4:
+      "පේරාදෙණිය විශ්වවිද්‍යාලයෙන් උපාධිය ලබා ඇති, ආචාර්ය චරිත් මුනසිංහට ජීව විද්‍යාව ඉගැන්වීමේ සහ ශ්‍රී ලංකාවේ ප්‍රමුඛ රාජ්‍ය විශ්වවිද්‍යාලවලට සිසුන් ඇතුළත් කිරීමට උදව් කිරීමේ වසර 10 කට වැඩි පළපුරුද්දක් ඇත.",
+
+    title5: "",
+    description5:
+      "ඔහුගේ කැපවීම සහ විශේෂඥතාවය ඔහුව උසස් පෙළ සිසුන් සඳහා විශ්වාසනීය උපදේශකයෙකු කර ඇත.",
   },
 };
 
